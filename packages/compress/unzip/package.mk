04678c69280f82c9ef455d5bc93c6e5850232a4c
Ian Leonard <antonlacon@gmail.com>
2019-02-18 20:27:42 +0000

    compress: add variables for USCAN support
    Signed-off-by: Ian Leonard <antonlacon@gmail.com>

--- packages/compress/unzip/package.mk
+++ packages/compress/unzip/package.mk
@@ -1,11 +1,12 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+# Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="unzip"
 PKG_VERSION="60"
 PKG_SHA256="036d96991646d0449ed0aa952e4fbe21b476ce994abc276e49d30e686708bd37"
 PKG_LICENSE="OSS"
-PKG_SITE="http://www.info-zip.org/pub/infozip/"
+PKG_SITE="http://infozip.sourceforge.net/UnZip.html"
 PKG_URL="http://ftp.uk.i-scream.org/sites/www.ibiblio.org/gentoo/distfiles/$PKG_NAME$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="UnZip is an extraction utility for archives compressed in .zip format."
--- packages/compress/zlib/package.mk
+++ packages/compress/zlib/package.mk
@@ -1,11 +1,13 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+# Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="zlib"
 PKG_VERSION="1.2.11"
 PKG_SHA256="4ff941449631ace0d4d203e3483be9dbc9da454084111f97ea0a2114e19bf066"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.zlib.net"
+PKG_WATCH="${PKG_SITE} ${PKG_NAME}-(.+)\.tar\.(?:gz|bz2|xz)"
 PKG_URL="http://zlib.net/$PKG_NAME-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_HOST="cmake:host"
 PKG_DEPENDS_TARGET="toolchain"
