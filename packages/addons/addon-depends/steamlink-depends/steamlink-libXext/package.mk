3714ebc377bf80b86c3f585dc6e3d6b2af0b1702
MilhouseVH <milhouseVH.github@nmacleod.com>
2020-03-08 15:45:17 +0000

    steamlink-rpi: add autoremove fixes

--- packages/addons/addon-depends/steamlink-depends/steamlink-libXext/package.mk
+++ packages/addons/addon-depends/steamlink-depends/steamlink-libXext/package.mk
@@ -7,12 +7,9 @@ PKG_NAME="steamlink-libXext"
 PKG_LONGDESC="libXext for steamlink-rpi"
 PKG_URL=""
 PKG_DEPENDS_TARGET+=" libXext"
+PKG_BUILD_FLAGS+=" -sysroot"
 
 unpack() {
   mkdir -p ${PKG_BUILD}
   tar --strip-components=1 -xf ${SOURCES}/${PKG_NAME:10}/${PKG_NAME:10}-${PKG_VERSION}.tar.bz2 -C ${PKG_BUILD}
-}
-
-makeinstall_target() {
-  :
 }
