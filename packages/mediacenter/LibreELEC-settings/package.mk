6a5034073a49f2b2b67bbf61228dcf0e030e8aed
Ian Leonard <antonlacon@gmail.com>
2020-06-29 03:25:39 +0000

    temp: use my le settings repo for addon
    Signed-off-by: Ian Leonard <antonlacon@gmail.com>

--- packages/mediacenter/LibreELEC-settings/package.mk
+++ packages/mediacenter/LibreELEC-settings/package.mk
@@ -3,11 +3,11 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="LibreELEC-settings"
-PKG_VERSION="12b47f4ab54fefd755507cf555c33771b8bc7ed3"
-PKG_SHA256="8dd30acd745b1ebda0ebfbe9b893253d33c7750ff52eb243f25f8bd59d3f14be"
+PKG_VERSION="59ce942064af55c944aa34eccf1abdf4497f5825"
+PKG_SHA256="9015c389474f4b8444bb064b26908fd1b1ef1d36d9bf9751a8603f3e62b1548d"
 PKG_LICENSE="GPL"
 PKG_SITE="https://libreelec.tv"
-PKG_URL="https://github.com/LibreELEC/service.libreelec.settings/archive/${PKG_VERSION}.tar.gz"
+PKG_URL="https://github.com/antonlacon/service.libreelec.settings/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain Python3 connman dbussy"
 PKG_LONGDESC="LibreELEC-settings: is a settings dialog for LibreELEC"
 
