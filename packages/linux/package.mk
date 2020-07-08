f881716c2915d15c5331f8f58e78a6caa26a2236
Ian Leonard <antonlacon@gmail.com>
2020-07-08 03:28:24 +0000

    rpi: linux: add raspberrypi64 option and use it
    With a separate config being maintained out of tree, it's easier
    to keep track of my own kernel upgrades.
    
    Signed-off-by: Ian Leonard <antonlacon@gmail.com>

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -29,6 +29,13 @@ case "${LINUX}" in
     PKG_SOURCE_NAME="linux-${LINUX}-${PKG_VERSION}.tar.gz"
     PKG_PATCH_DIRS="raspberrypi rtlwifi/6.13 rtlwifi/6.14 rtlwifi/6.15 rtlwifi/6.17"
     ;;
+  raspberrypi64)
+    PKG_VERSION="c575ed8cd074b02e356e87269a544dbffe1a2c84" # 6.6.16
+    PKG_SHA256="6ce7c5866b75d8d6fc857f658098e9a1b6541ad8bead2675c0e9af42debdfc03"
+    PKG_URL="https://github.com/raspberrypi/linux/archive/${PKG_VERSION}.tar.gz"
+    PKG_SOURCE_NAME="linux-${LINUX}-${PKG_VERSION}.tar.gz"
+    PKG_PATCH_DIRS="raspberrypi"
+    ;;
   *)
     PKG_VERSION="6.16.3"
     PKG_SHA256="80439ba055c12f541abf44b8fc3c9b825a8f42fc25ce67462ec7e556c5790b85"
