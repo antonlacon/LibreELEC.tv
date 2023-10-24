2d403106a60a41f03326b83908da589481f7e0fd
KOPRajs <zdenek.koprivik@post.cz>
2023-10-24 12:00:24 +0200

    kodi-binary-addons: add SwanStation game add-on

---
+++ packages/mediacenter/kodi-binary-addons/game.libretro.swanstation/package.mk
@@ -0,0 +1,17 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="game.libretro.swanstation"
+PKG_VERSION="1.0.0.27-Nexus"
+PKG_SHA256="05c497cf6862680694fb430ca311afebdeea63ef6f8ae94eae7194ecc25ad20c"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/kodi-game/game.libretro.swanstation"
+PKG_URL="https://github.com/kodi-game/game.libretro.swanstation/archive/${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain kodi-platform libretro-swanstation"
+PKG_SECTION=""
+PKG_LONGDESC="game.libretro.swanstation: SwanStation for Kodi"
+
+PKG_IS_ADDON="yes"
+PKG_ADDON_TYPE="kodi.gameclient"
