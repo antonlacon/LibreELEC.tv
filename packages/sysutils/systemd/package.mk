e87cc43ecb02284a43af9373fa42ad4294c35ccf
Ian Leonard <antonlacon@gmail.com>
2025-06-29 20:30:21 -0400

    Revert "systemd: enable timedated"
    This reverts commit 2a67f047fe388d90a2327c9995379bf2f5326586.
    
    timedated commit kept for testing

--- packages/sysutils/systemd/package.mk
+++ packages/sysutils/systemd/package.mk
@@ -66,7 +66,7 @@ PKG_MESON_OPTS_TARGET="--libdir=/usr/lib \
                        -Dnologin-path=/usr/sbin/nologin \
                        -Dhomed=disabled \
                        -Dnetworkd=false \
-                       -Dtimedated=true \
+                       -Dtimedated=false \
                        -Dtimesyncd=true \
                        -Dfirstboot=false \
                        -Drandomseed=false \
@@ -174,6 +174,9 @@ post_makeinstall_target() {
   else
     find_file_path scripts/nspawn-symlink && cp -PRv ${FOUND_PATH} ${INSTALL}/usr/bin
   fi
+
+  # remove timedatectl
+  safe_remove ${INSTALL}/usr/bin/timedatectl
 
   # remove unneeded generators
   for gen in ${INSTALL}/usr/lib/systemd/system-generators/*; do
@@ -324,7 +327,6 @@ post_install() {
   enable_service envconfig.service
   enable_service cpufreq.service
   enable_service network-base.service
-  enable_service systemd-timedated.service
   enable_service systemd-timesyncd.service
   enable_service systemd-timesyncd-setup.service
   enable_service systemd-userdbd.socket
