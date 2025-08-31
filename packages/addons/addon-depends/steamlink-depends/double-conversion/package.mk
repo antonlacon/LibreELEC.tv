5533f0859beb00a884ce209506a5a01eafae1ec6
Ian Leonard <antonlacon@gmail.com>
2025-08-31 01:27:19 -0400

    double-conversion: update to 3.3.1
    Signed-off-by: Ian Leonard <antonlacon@gmail.com>

--- packages/addons/addon-depends/steamlink-depends/double-conversion/package.mk
+++ packages/addons/addon-depends/steamlink-depends/double-conversion/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2024-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="double-conversion"
-PKG_VERSION="3.3.0"
-PKG_SHA256="04ec44461850abbf33824da84978043b22554896b552c5fd11a9c5ae4b4d296e"
+PKG_VERSION="3.3.1"
+PKG_SHA256="fe54901055c71302dcdc5c3ccbe265a6c191978f3761ce1414d0895d6b0ea90e"
 PKG_LICENSE="BSD"
 PKG_SITE="https://github.com/google/double-conversion"
 PKG_URL="https://github.com/google/double-conversion/archive/refs/tags/v${PKG_VERSION}.tar.gz"
@@ -12,5 +12,6 @@ PKG_LONGDESC="Efficient binary-decimal and decimal-binary conversion routines fo
 PKG_TOOLCHAIN="cmake"
 PKG_BUILD_FLAGS="-sysroot"
 
-PKG_CMAKE_OPTS_TARGET="-DBUILD_SHARED_LIBS=ON \
+PKG_CMAKE_OPTS_TARGET="-DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
+                       -DBUILD_SHARED_LIBS=ON \
                        -DBUILD_TESTING=OFF"
