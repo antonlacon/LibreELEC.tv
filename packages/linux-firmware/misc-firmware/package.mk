4d570645d78b3b6d151cae2bd994bf97592c877d
Ian Leonard <antonlacon@gmail.com>
2026-02-08 00:32:36 -0500

    misc-firmware: drop toolchain depend
    Signed-off-by: Ian Leonard <antonlacon@gmail.com>

--- packages/linux-firmware/misc-firmware/package.mk
+++ packages/linux-firmware/misc-firmware/package.mk
@@ -8,7 +8,7 @@ PKG_SHA256="862a5a62b9794d7c6753ac0f7ae03fd34ee2564199afd61c42dc8f69d4c5b876"
 PKG_LICENSE="Free-to-use"
 PKG_SITE="https://github.com/LibreELEC/misc-firmware"
 PKG_URL="https://github.com/LibreELEC/misc-firmware/archive/${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="toolchain kernel-firmware"
+PKG_DEPENDS_TARGET="kernel-firmware"
 PKG_LONGDESC="misc-firmware: firmwares for various drivers"
 PKG_TOOLCHAIN="manual"
 
