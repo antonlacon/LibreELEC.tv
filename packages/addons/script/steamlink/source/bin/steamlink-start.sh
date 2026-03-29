8e8eb51a9e6c67f8a148942c97d2238c8630f551
Ian Leonard <antonlacon@gmail.com>
2026-03-28 21:42:44 -0400

    steamlink:steamlink-start.sh: refactor
    Signed-off-by: Ian Leonard <antonlacon@gmail.com>

--- packages/addons/script/steamlink/source/bin/steamlink-start.sh
+++ packages/addons/script/steamlink/source/bin/steamlink-start.sh
@@ -6,32 +6,58 @@
 . /etc/profile
 oe_setup_addon script.program.steamlink
 
-# Steamlink not ready; abort
-if [ ! -f ${ADDON_DIR}/prep.ok ]; then
+# Abort if Steam Link is not setup
+if [ ! -f "${ADDON_DIR}/prep.ok" ]; then
   exit 0
 fi
 
-# Adapt to meet steamlink requirements
+UDEV_DIR="/lib/udev/rules.d"
+UPPER="${ADDON_DIR}/steamlink/udev/rules.d"
+WORK="${ADDON_DIR}/steamlink/.overlay"
 
-# mount an overlay for udev rules and reload udev rules
-if [ $(grep -c "/lib/udev/rules.d" /proc/mounts) -eq 0 ]; then
-  if [ ! -d ${ADDON_DIR}/steamlink/.overlay ]; then
-    mkdir -p ${ADDON_DIR}/steamlink/.overlay
+# Cleanup routine on exit
+cleanup() {
+  # Unount udev overlay
+  if mountpoint -q "${UDEV_DIR}"; then
+    umount "${UDEV_DIR}"
+    udevadm trigger
   fi
-  mount -t overlay overlay -o lowerdir=/lib/udev/rules.d,upperdir=${ADDON_DIR}/steamlink/udev/rules.d/,workdir=${ADDON_DIR}/steamlink/.overlay /lib/udev/rules.d
+
+  # Start Kodi if not running
+  if ! systemctl is-active --quiet kodi; then
+    systemctl start kodi
+  fi
+}
+trap cleanup EXIT
+
+for dir in "${UPPER}" "${WORK}"; do
+  if [ ! -d "${dir}" ]; then
+    mkdir -p "${dir}" || {
+      echo "Error: Failed to create directory: ${dir}" >&2
+      exit 1
+    }
+  fi
+done
+
+# Mount overlay if not mounted
+if ! mountpoint -q "${UDEV_DIR}"; then
+  mount -t overlay overlay \
+    -o "lowerdir=${UDEV_DIR},upperdir=${UPPER},workdir=${WORK}" \
+    "${UDEV_DIR}"
+
   udevadm trigger
 fi
 
-# Launch steamlink
-# controller input goes to kodi if it is running
-systemctl stop kodi
-# xxx: assume alsa audio by default
+# Stop Kodi so controller input goes to Steam Link
+systemctl stop kodi || echo "Warning: Kodi do not stop cleanly"
+
+# Audio environment for Steam Link
+# xxx: assumes ALSA
 export PULSE_SERVER="none"
 export SDL_AUDIODRIVER="alsa"
-${ADDON_DIR}/steamlink/steamlink.sh
-# Cleanup
-umount /lib/udev/rules.d
-udevadm trigger
-systemctl start kodi
 
-exit 0
+# Launch Steam Link
+"${ADDON_DIR}/steamlink/steamlink.sh"
+STATUS=$?
+
+exit $STATUS
